e180c19e50ddca057e646af215f316115694f34f
ro69alik <osetrov_2006_sn@mail.ru>
2025-12-04 09:19:48 +0500

    feat: InventoryItemModel public getters

--- Witches Debt/Assets/Scripts/UI/InventoryModel.cs
+++ Witches Debt/Assets/Scripts/UI/InventoryModel.cs
@@ -1,14 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class InventoryModel
 {
     private List<InventoryItemConfig> storage;
-    private Dictionary<SpellType, List<InventoryItemConfig>> spells;
+    private Dictionary<SpellType, List<InventoryItemConfig>> spellsStorages;
     private int storageCapacity;
     private int spellModsCapasity;
+
+    public List<InventoryItemConfig> Storage() => storage;
+    public List<SpellType> Spells() => spellsStorages.Keys.ToList();
+    public List<InventoryItemConfig> SpellStorage(SpellType spellType) => spellsStorages[spellType];
+    public Dictionary<SpellType, List<InventoryItemConfig>> SpellsStorages() => spellsStorages;
+    public int StorageCapacity() => storageCapacity;
+    public int SpellModsCapacity() => spellModsCapasity;
 
     public bool TryAddItemToInventory(InventoryItemConfig item)
     {
