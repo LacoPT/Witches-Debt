1cd4be4be661b55ea494e3c33ebcede02a461079
LacoPT <izatullinkirill@gmail.com>
2025-12-27 04:39:27 +0500

    fix: DotHelper scope set

--- Witches Debt/Assets/Scripts/DI/GameInstaller.cs
+++ Witches Debt/Assets/Scripts/DI/GameInstaller.cs
@@ -6,7 +6,7 @@ public class GameInstaller : MonoInstaller
     [SerializeField] private ModRarityDistribution modRarityDistribution;
     public override void InstallBindings()
     {
-        Container.Bind<DotHelper>().FromNewComponentOnNewGameObject();
+        Container.Bind<DotHelper>().FromNewComponentOnNewGameObject().AsSingle();
         Container.Bind<PlayerTargetProvider>().AsSingle();
         Container.Bind<EnemyRegistry>().AsSingle();
     }
